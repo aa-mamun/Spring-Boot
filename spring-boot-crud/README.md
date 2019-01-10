5e6fb11235500a2368263670e5c64303c962382c
Abdullah Al Mamun <mamun.ofcpro@gmail.com>
2019-01-10 12:47:01 +0600

    spring-boot-crud project initiate

---
+++ spring-boot-crud/README.md
@@ -0,0 +1,6 @@
+## Spring Boot Crud Example
+### Technolgy used 
+    - Spring Boot
+    - Spring Data
+    - Thymeleaf
+    - mysql	
---
+++ spring-boot-crud/src/main/java/com/mamun/springbootcrud/domain/User.java
@@ -0,0 +1,44 @@
+package com.mamun.springbootcrud.domain;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private Long userId;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
