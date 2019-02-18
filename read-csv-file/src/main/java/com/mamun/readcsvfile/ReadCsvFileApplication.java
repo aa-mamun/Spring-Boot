cfdf34842b98eac662b64cc7824c2aaee699a14c
Abdullah Al Mamun <mamun.ofcpro@gmail.com>
2019-02-18 12:42:52 +0600

    demo project to read csv file

---
+++ read-csv-file/src/main/java/com/mamun/readcsvfile/ReadCsvFileApplication.java
@@ -0,0 +1,13 @@
+package com.mamun.readcsvfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadCsvFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadCsvFileApplication.class, args);
+    }
+
+}
